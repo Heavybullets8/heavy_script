993475135b9394b2fb104a551e4198b768240c73
Heavybullets8 <heavybullets8@gmail.com>
2022-06-13 15:59:42 -0600

    test

--- heavy_script.sh
+++ heavy_script.sh
@@ -3,6 +3,7 @@
 #If no argument is passed, kill the script.
 [[ -z "$*" || "-" == "$*" || "--" == "$*"  ]] && echo "This script requires an argument, use --help for help" && exit
 
+echo "yep"
 
 args=("$@")
 

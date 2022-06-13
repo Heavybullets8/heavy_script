83b1e286e800ff82212e5dd0cd2ba7f7eb7f4925
Heavybullets8 <heavybullets8@gmail.com>
2022-06-13 15:10:24 -0600

    test

--- heavy_script.sh
+++ heavy_script.sh
@@ -4,6 +4,7 @@
 [[ -z "$*" || "-" == "$*" || "--" == "$*"  ]] && echo "This script requires an argument, use --help for help" && exit
 
 
+
 ARGS=$(echo "$*" | sed -E 's/[[:space:]]?--self-update[[:space:]]?//g')
 
 SCRIPT=$(readlink -f "$0")

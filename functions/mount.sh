dba0e738e6ec96d7f6f44299c71104b322f13ec4
Heavybullets8 <heavybullets8@gmail.com>
2022-07-29 01:53:05 -0600

    test

--- functions/mount.sh
+++ functions/mount.sh
@@ -16,10 +16,10 @@ do
             exit
             ;;
         1)
-            call=$(k3s kubectl get pvc -A | sort -u | awk '{print $1 "\t" $2 "\t" $4}' | column -t | sed "s/^0/ /")
+            call=$(k3s kubectl get pvc -A | sort -u | awk '{print $1 "\t" $2 "\t" $4}' | sed "s/^0/ /")
             mount_list=$(echo "$call" | sed 1d | nl -s ") ")
             mount_title=$(echo "$call" | head -n 1)
-            list=$(echo -e "$mount_title\n$mount_list")
+            list=$(echo -e "$mount_title\n$mount_list" | column -t)
 
             while true
             do

c52e1e86f930c1ba6f0bddc39904f880580668b4
Heavybullets8 <heavybullets8@gmail.com>
2022-08-04 21:46:37 -0600

    test

--- functions/update_apps.sh
+++ functions/update_apps.sh
@@ -29,12 +29,16 @@ do
         until [[ "$proc_count" -ge "$update_limit" || $it -ge ${#array[@]} ]]
         do
             update_apps "${array[$it]}" &
-            sleep 4
             processes+=($!)
             ((it++))
             ((proc_count++))
         done
         ((ttl++))
+        if [[ $ttl -eq 1 ]]; then
+            sleep 15
+        else
+            sleep 6
+        fi
     elif [[ $proc_count != 0 ]]; then # Wait for all processes to finish
         sleep 6
     else # All processes must be completed, break out of loop

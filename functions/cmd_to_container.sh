3b90402416a1719c660396fc06d415c42ce5668f
Heavybullets8 <heavybullets8@gmail.com>
2022-08-12 09:54:59 -0600

    test

--- functions/cmd_to_container.sh
+++ functions/cmd_to_container.sh
@@ -27,8 +27,8 @@ search=$(k3s crictl ps -a -s running | sed -E 's/[[:space:]]([0-9]*|About)[a-z0-
 mapfile -t pod_id < <(echo "$search" | grep -E "[[:space:]]$app_name([[:space:]]|-([-[:alnum:]])*[[:space:]])" | awk '{print $(NF)}')
 for pod in "${pod_id[@]}"
 do
-    printf '%s\0' "${containers[@]}" | grep -Fxqz -- "$(echo "$search" | grep "$pod" | awk '{print $4}')" && continue 
-    containers+=("$(echo "$search" | grep "$pod" | awk '{print $4}')")
+    printf '%s\0' "${containers[@]}" | grep -Fxqz -- "$(echo "$search" | grep "$pod" | awk '{print $4}' | tr -d " \t\r ")" && continue 
+    containers+=("$(echo "$search" | grep "$pod" | awk '{print $4}' | tr -d " \t\r ")")
 done
 case "${#containers[@]}" in
     0)

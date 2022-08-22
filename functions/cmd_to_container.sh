8b60029b375d63879e3b29be2970c9b862f11d2a
Heavybullets8 <heavybullets8@gmail.com>
2022-08-21 22:11:35 -0600

    echo containers

--- functions/cmd_to_container.sh
+++ functions/cmd_to_container.sh
@@ -34,7 +34,7 @@ do
     printf '%s\0' "${containers[@]}" | grep -Fxqz -- "$(echo "$search" | grep "$pod" | awk '{print $4}' | tr -d " \t\r ")" && continue 
     containers+=("$(echo "$search" | grep "$pod" | awk '{print $4}' | tr -d " \t\r ")")
 done
-mapfile -t containers < <("${containers[@]}")
+mapfile -t containers < <("echo ${containers[*]}")
 case "${#containers[@]}" in
     0)
         echo -e "No containers available\nAre you sure the application in running?"

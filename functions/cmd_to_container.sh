d930c734f733f46cd9f5b59e78fb6dae2bdcde98
Heavybullets8 <heavybullets8@gmail.com>
2022-08-21 22:36:24 -0600

    remove error message on STOPPED application

--- functions/cmd_to_container.sh
+++ functions/cmd_to_container.sh
@@ -35,7 +35,7 @@ do
     # printf '%s\0' "${containers[@]}" | grep -Fxqz -- "$(echo "$search" | grep "$pod" | awk '{print $4}' | tr -d " \t\r ")" && continue 
     echo "$search" | grep "$pod" | awk '{print $4}' | tr -d " \t\r " >> cont_file
 done
-mapfile -t containers < <(sort -u cont_file)
+mapfile -t containers < <(sort -u cont_file 2> /dev/null)
 rm cont_file 2> /dev/null
 case "${#containers[@]}" in
     0)

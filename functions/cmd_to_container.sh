a0037cb4d095312f9387ae37aa9ac82e0025c89a
Heavybullets8 <heavybullets8@gmail.com>
2022-08-21 21:43:01 -0600

    read IFS

--- functions/cmd_to_container.sh
+++ functions/cmd_to_container.sh
@@ -34,7 +34,7 @@ do
     printf '%s\0' "${containers[@]}" | grep -Fxqz -- "$(echo "$search" | grep "$pod" | awk '{print $4}' | tr -d " \t\r ")" && continue 
     containers+="$(echo "$search" | grep "$pod" | awk '{print $4}' | tr -d " \t\r ")"
 done
-readarray -t containers <<<"$containers"
+IFS=" " read -r -a containers <<< "$containers"
 case "${#containers[@]}" in
     0)
         echo -e "No containers available\nAre you sure the application in running?"

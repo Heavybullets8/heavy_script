cd836ba537a77d81f9a7813112c72b476f40acff
Heavybullets8 <heavybullets8@gmail.com>
2022-08-23 13:02:09 -0600

    Formatting, only pull CL in curl

--- functions/self_update.sh
+++ functions/self_update.sh
@@ -4,22 +4,22 @@ args=("$@")
 self_update() {
 
 latest_ver=$(git describe --tags "$(git rev-list --tags --max-count=1)")
-git fetch --tags &> /dev/null 
+git fetch --tags &>/dev/null 
 echo "🅂 🄴 🄻 🄵"
 echo "🅄 🄿 🄳 🄰 🅃 🄴"
 if  [[ "$hs_version" != "$latest_ver" ]] ; then
     echo "Found a new version of HeavyScript, updating myself..."
-    git checkout "$latest_ver"
+    git checkout "$latest_ver" &>/dev/null 
     count=0
     for i in "${args[@]}"
     do
         [[ "$i" == "--self-update" ]] && unset "args[$count]" && break
         ((count++))
     done
-    echo "Updating from:"
-    echo "$hs_version"
-    echo "Updating To:"
-    curl --silent "https://api.github.com/repos/HeavyBullets8/heavy_script/releases/latest" | jq -r .tag_name,.body
+    echo "Updating from: $hs_version"
+    echo "Updating To: $latest_ver"
+    echo "Changelog:"
+    curl --silent "https://api.github.com/repos/HeavyBullets8/heavy_script/releases/latest" | jq -r .body
     [[ -z ${args[*]} ]] && echo -e "No more arguments, exiting..\n\n" && exit
     echo -e "Running the new version...\n\n"
     sleep 5

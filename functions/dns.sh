2c8c24e2373c31325f9b5a89fa8702517a984602
Heavybullets8 <heavybullets8@gmail.com>
2022-08-31 22:36:55 -0600

    remove extra hyphen from name

--- functions/dns.sh
+++ functions/dns.sh
@@ -22,7 +22,7 @@ do
     appName=$(grep -E "\s$i\s" "dns_file" | awk '{print $3}' | sed 's/-[^-]*-[^-]*$//' | sed 's/-0//' | head -n 1)
     port=$(echo "$all_ports" | grep -E "\s$appName\s" | awk '{print $6}' | grep -Eo "^[[:digit:]]+{1}")
     echo -e "$appName $appName.$i.svc.cluster.local $port"
-done | nl -s ") " -b t | sed '0,/\s\s\s/{s/\s\s\s/- ----- -------- ----/}'| column -t -N "#,Name,DNS_Name,Port"
+done | nl -s ") " -b t | sed '0,/\s\s\s/{s/\s\s\s/- ---- -------- ----/}'| column -t -N "#,Name,DNS_Name,Port"
 rm dns_file
 }
 export -f dns

a3c052dce6a562bdbb2b817e3574f8f9da8b76c9
Heavybullets8 <heavybullets8@gmail.com>
2022-12-15 07:10:21 +0000

    reverse gt symbol

--- functions/misc.sh
+++ functions/misc.sh
@@ -19,7 +19,7 @@ export -f sync
 prune(){
 echo -e "🄿 🅁 🅄 🄽 🄴"  
 version="$(cli -c 'system version' | awk -F '-' '{print $3}' |  tr -d " \t\r\.")"
-if (( "$version" <= 22120 )); then
+if (( "$version" >= 22120 )); then
     if ! cli -c 'app container config prune prune_options={"remove_unused_images": true, "remove_stopped_containers": true}' | head -n -4; then
         echo "Failed to Prune Docker Images"
     fi

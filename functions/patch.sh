c4f457b2a5e930efb1eb9ecaba54b3e9edd5e4fc
Heavybullets8 <heavybullets8@gmail.com>
2022-12-28 15:13:30 -0700

    hotfix for upstream instability

--- functions/patch.sh
+++ functions/patch.sh
@@ -14,34 +14,21 @@ patch_2212_backups(){
     echo
 
 
-    #Download patch
-    echo "Downloading Backup Patch"
-    if ! wget -q https://github.com/truecharts/truetool/raw/main/hotpatch/2212/HP1.patch; then
-        echo "Failed to download Backup Patch"
-        exit
-    else
-        echo "Downloaded Backup Patch"
-    fi
-
-    echo
-
     # Apply patch
     echo "Applying Backup Patch"
 
     # Check if patch has already been applied
-    if ! patch -R --dry-run -N -s -p0 -d /usr/lib/python3/dist-packages/middlewared/ < HP1.patch &>/dev/null; then
+    if ! patch -R --dry-run -N -s -p0 -d /usr/lib/python3/dist-packages/middlewared/ < patches/restore.patch &>/dev/null; then
         # If patch has not been applied, apply it
-        if patch -N --reject-file=/dev/null -s -p0 -d /usr/lib/python3/dist-packages/middlewared/ < HP1.patch &>/dev/null; then
-            echo "Backup Patch applied"
-            rm -rf HP1.patch
+        if patch -N --reject-file=/dev/null -s -p0 -d /usr/lib/python3/dist-packages/middlewared/ < patches/restore.patch &>/dev/null; then
+            echo "Restore Patch applied"
         else
             # If patch fails to apply, exit
             echo "Error applying Backup Patch"
             exit 1
         fi
     else
-        echo "Backup Patch already applied"
-        rm -rf HP1.patch
+        echo "Restore Patch already applied"
         exit 0
     fi
 

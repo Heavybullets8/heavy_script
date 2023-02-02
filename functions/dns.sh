9de25e03b906ccd5ecaa21682886aad3b4ab6b54
Heavybullets8 <heavybullets8@gmail.com>
2023-02-02 18:53:56 +0000

    Alt colors dns (#49)
    - Add alternating colors to DNS output for easier readability

--- functions/dns.sh
+++ functions/dns.sh
@@ -6,22 +6,35 @@ dns(){
     echo -e "${blue}Generating DNS Names..${reset}"
 
     # Pulling pod names
-    k3s crictl pods --namespace ix -s Ready | sed -E 's/[[:space:]]([0-9]*|About)[a-z0-9 ]{5,12}ago[[:space:]]//' | grep -v 'svclb-' | sed '1d'  >> dns_file
+    if ! k3s crictl pods --namespace ix -s Ready | 
+        sed -E 's/[[:space:]]([0-9]*|About)[a-z0-9 ]{5,12}ago[[:space:]]//' |
+        grep -v 'svclb-' |
+        sed '1d' > dns_file; then
+        echo "Error: failed to retrieve pod names" >&2
+        return 1
+    fi
     mapfile -t ix_name_array < <(< dns_file awk '{print $4}' | sort -u )
-
+    
     # Pulling all ports
-    all_ports=$(k3s kubectl get service -A)
+    if ! all_ports=$(k3s kubectl get service -A); then
+        echo "Error: failed to retrieve port information" >&2
+        return 1
+    fi
 
     clear -x
-    count=0
     for i in "${ix_name_array[@]}"
     do
-        [[ count -le 0 ]] && echo -e "\n" && ((count++))
         full_app_name=$(grep -E "\s$i\s" "dns_file" | awk '{print $3}' | sed 's/-[^-]*-[^-]*$//' | sed 's/-0//' | head -n 1)
         app_name=$(echo "$i" | cut -c 4-)
         port=$(echo "$all_ports" | grep -E "\s$full_app_name\s" | awk '{print $6}' | grep -Eo "^[[:digit:]]+{1}")
-        echo -e "$app_name $full_app_name.$i.svc.cluster.local $port"
-    done | nl -s ") " -b t | sed '0,/\s\s\s/{s/\s\s\s/- ---- -------- ----/}'| column -t -N "#,Name,DNS_Name,Port"
+        count=$((count + 1))
+        if (( count % 2 == 0 )); then
+            color="\033[0m"
+        else
+            color="\033[1;30m"
+        fi
+        echo -e "${color}$app_name $full_app_name.$i.svc.cluster.local $port\033[0m"
+    done | column -t -N "App Name,DNS Name,Port"
     rm dns_file
 }
 export -f dns

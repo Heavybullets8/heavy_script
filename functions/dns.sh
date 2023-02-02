45b62892dcda899cdf04f31ed31642b3d4e661b5
Heavybullets8 <heavybullets8@gmail.com>
2023-02-02 11:43:48 -0700

    add colors

--- functions/dns.sh
+++ functions/dns.sh
@@ -6,28 +6,29 @@ dns(){
     echo -e "${blue}Generating DNS Names..${reset}"
 
     # Pulling pod names
-    k3s crictl pods --namespace ix -s Ready | sed -E 's/[[:space:]]([0-9]*|About)[a-z0-9 ]{5,12}ago[[:space:]]//' | grep -v 'svclb-' | sed '1d'  >> dns_file
+    k3s crictl pods --namespace ix -s Ready | 
+        sed -E 's/[[:space:]]([0-9]*|About)[a-z0-9 ]{5,12}ago[[:space:]]//' |
+        grep -v 'svclb-' |
+        sed '1d' > dns_file
     mapfile -t ix_name_array < <(< dns_file awk '{print $4}' | sort -u )
 
     # Pulling all ports
     all_ports=$(k3s kubectl get service -A)
 
     clear -x
-    count=0
     for i in "${ix_name_array[@]}"
     do
-        [[ count -le 0 ]] && echo -e "\n"
         full_app_name=$(grep -E "\s$i\s" "dns_file" | awk '{print $3}' | sed 's/-[^-]*-[^-]*$//' | sed 's/-0//' | head -n 1)
         app_name=$(echo "$i" | cut -c 4-)
         port=$(echo "$all_ports" | grep -E "\s$full_app_name\s" | awk '{print $6}' | grep -Eo "^[[:digit:]]+{1}")
-        if ((count % 2 == 0)); then
-            echo -e "\033[90m$app_name $full_app_name.$i.svc.cluster.local $port${reset}"
+        count=$((count + 1))
+        if (( count % 2 == 0 )); then
+            color="\033[0m"
         else
-            echo -e "${reset}$app_name $full_app_name.$i.svc.cluster.local $port${reset}"
+            color="\033[1;30m"
         fi
-        ((count++))
-    done | nl -s ") " -b t | sed '0,/\s\s\s/{s/\s\s\s/- ---- -------- ----/}'| column -t -N "#,Name,DNS_Name,Port"
+        echo -e "${color}$app_name $full_app_name.$i.svc.cluster.local $port\033[0m"
+    done | column -t -N "App Name,DNS Name,Port"
     rm dns_file
-
 }
 export -f dns

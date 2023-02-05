e02fe9c31e2dd6093adbf70f21925be1ae8bcfe5
Heavybullets8 <heavybullets8@gmail.com>
2023-02-05 23:44:45 +0000

    One liner improvements (#63)
    - Change output colors
    - Add better error messages
    - Silence git clone info
    - Change the one liner command to source files

--- functions/deploy.sh
+++ functions/deploy.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+# colors
+reset='\033[0m'
+red='\033[0;31m'
+yellow='\033[1;33m'
+green='\033[0;32m'
+blue='\033[0;34m'
+
 
 # Check user's permissions 
 if [[ $(id -u) != 0 ]]; then
-    echo "This script must be run as root." >&2
+    echo -e "${red}This script must be run as root.${reset}" >&2
     exit 1
 fi
 
@@ -12,12 +19,15 @@ update_repo() {
     cd "$script_dir" || return 1
     git reset --hard &>/dev/null
     git fetch --tags &>/dev/null
+    echo
+    echo -e "${blue}Checking out the latest release...${reset}"
     if ! git checkout --force "$(git describe --tags "$(git rev-list --tags --max-count=1)")" &>/dev/null; then
-        echo "Failed to check out the latest release."
+        echo "${red}Failed to check out the latest release.${reset}"
         return 1
+    else
+        echo -e "${green}Successfully checked out the latest release.${reset}"
+        return 0
     fi
-    echo "Successfully updated the repository"
-    return 0
 }
 
 
@@ -30,31 +40,37 @@ script_wrapper="$bin_dir/$script_name"
 
 # Check if the script repository already exists
 if [[ -d "$script_dir" ]]; then
-    echo "The $script_name repository already exists."
+    echo -e "${yellow}The ${blue}$script_name${yellow} repository already exists.${reset}"
     if [[ -d "$script_dir/.git" ]]; then
-        echo "Reinstalling $script_name repository..."
+        echo -e "${blue}Reinstalling $script_name repository...${reset}"
         if ! update_repo "$script_dir"; then
-            echo "Failed to reinstall the repository"
+            echo -e "${red}Failed to reinstall the repository${reset}"
             exit 1
+        else
+            echo -e "${green}Successfully reinstalled the repository${reset}"
         fi
     else
         # Convert the directory into a git repository
-        echo "Converting it into a git repository..."
+        echo -e "${blue}Converting it into a git repository...${reset}"
         cd "$script_dir" || exit 1
         git init
         git remote add origin "https://github.com/Heavybullets8/heavy_script.git"
         if ! update_repo "$script_dir"; then
-            echo "Failed to convert to git repository"
+            echo "${red}Failed to convert to git repository${reset}"
             exit 1
+        else
+            echo -e "${green}Successfully converted to git repository${reset}"
         fi
     fi
 else
     # Clone the script repository
-    echo "Cloning $script_name repository..."
+    echo -e "${blue}Cloning $script_name repository...${reset}"
     cd "$HOME" || exit 1
-    if ! git clone "https://github.com/Heavybullets8/heavy_script.git"; then
-        echo "Failed to clone the repository"
+    if ! git clone "https://github.com/Heavybullets8/heavy_script.git" &>/dev/null; then
+        echo -e "${red}Failed to clone the repository${reset}"
         exit 1
+    else
+        echo -e "${green}Successfully cloned the repository${reset}"
     fi
 
     cd "$script_dir" || exit 1
@@ -63,28 +79,34 @@ else
     fi
 fi
 
+echo
 
 # Create the bin directory if it does not exist
 if [[ ! -d "$bin_dir" ]]; then
-    echo "Creating $bin_dir directory..."
+    echo -e "${blue}Creating $bin_dir directory...${reset}"
     mkdir "$bin_dir"
 fi
 
 # Create symlink inside bin, to the script
-echo "Creating $script_wrapper wrapper..."
+echo -e "${blue}Creating $script_wrapper wrapper...${reset}"
 ln -sf "$script_dir/bin/$script_name" "$script_wrapper"
 chmod +x "$script_dir/bin/$script_name"
 
+echo
 
 # Add $HOME/bin to PATH in .bashrc and .zshrc
 for rc_file in .bashrc .zshrc; do
     if [[ ! -f "$HOME/$rc_file" ]]; then
-        echo "Creating $HOME/$rc_file file..."
+        echo -e "${blue}Creating $HOME/$rc_file file...${reset}"
         touch "$HOME/$rc_file"
     fi
 
     if ! grep -q "$bin_dir" "$HOME/$rc_file"; then
-        echo "Adding $bin_dir to $rc_file..."
+        echo -e "${blue}Adding $bin_dir to $rc_file...${reset}"
         echo "export PATH=$bin_dir:\$PATH" >> "$HOME/$rc_file"
     fi
 done
+
+echo
+echo -e "${green}Successfully installed ${blue}$script_name${reset}"
+echo

71d89192bc23aefb83a37cdef39a374c9e0bdb75
heavybullets8 <heavybullets8@gmail.com>
2023-02-05 19:23:26 -0700

    remove newline

--- functions/app_management.sh
+++ functions/app_management.sh
@@ -5,7 +5,7 @@ apps=()
 list_applications(){
 
     # retrieve list of app names
-    mapfile -t apps < <(cli -q -m csv -c 'app chart_release query name' | tail -n +2 | sort)
+    mapfile -t apps < <(cli -m csv -c 'app chart_release query name' | tail -n +2 | sort)
 
     # print out list of app names with numbered options
     for i in "${!apps[@]}"; do

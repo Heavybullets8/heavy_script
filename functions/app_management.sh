1591f20f3cdf269123a121d5214613aae8f68974
heavybullets8 <heavybullets8@gmail.com>
2023-02-05 19:14:21 -0700

    uhh

--- functions/app_management.sh
+++ functions/app_management.sh
@@ -26,7 +26,7 @@ delete_app_prompt(){
         # validate user selection
         if [ "$app_index" -gt 0 ] && [ "$app_index" -le "${#apps[@]}" ]; then
             # retrieve selected app name
-            sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' <<< "selected_app=${apps[app_index-1]}"
+            selected_app="$(echo "${apps[app_index-1]}" | tr -d ' \n\r')"
             # delete app
             if cli -c "app chart_release delete release_name=\"$selected_app\""; then
                 echo -e "${green}App $selected_app deleted{reset}"
@@ -54,7 +54,7 @@ restart_app_prompt(){
         # validate user selection
         if [ "$app_index" -gt 0 ] && [ "$app_index" -le "${#apps[@]}" ]; then
             # retrieve selected app name
-            sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//' <<< "selected_app=${apps[app_index-1]}"
+            selected_app="$(echo "${apps[app_index-1]}" | tr -d ' \n\r')"
 
             # delete app
             if cli -c "app chart_release delete release_name=\"$selected_app\""; then

039d7b755b11a037891d691f58d854d05516aae7
heavybullets8 <heavybullets8@gmail.com>
2023-02-10 16:22:55 -0700

    add reprompt

--- functions/app_management.sh
+++ functions/app_management.sh
@@ -10,20 +10,20 @@ prompt_app_selection() {
     case "$1" in
         "ALL")
             mapfile -t apps < <(cli -m csv -c 'app chart_release query name,status' | tail -n +2 | sort | tr -d " \t\r" | awk 'NF')
-            search="Any"
+            search_type="Any"
             ;;
         "STOPPED")
             mapfile -t apps < <(cli -m csv -c 'app chart_release query name,status' | tail -n +2 | sort | tr -d " \t\r" | awk 'NF' | grep "STOPPED")
-            search="STOPPED"
+            search_type="STOPPED"
             ;;
         "ACTIVE")
             mapfile -t apps < <(cli -m csv -c 'app chart_release query name,status' | tail -n +2 | sort | tr -d " \t\r" | awk 'NF' | grep "ACTIVE")
-            search="ACTIVE"
+            search_type="ACTIVE"
             ;;
     esac
 
     if [ "${#apps[@]}" -eq 0 ]; then
-        echo -e "${yellow}Application type: ${blue}$search${reset}"
+        echo -e "${yellow}Application type: ${blue}$search_type${reset}"
         echo -e "${yellow}Not found..${reset}"
         exit 1
     fi
@@ -53,119 +53,173 @@ prompt_app_selection() {
 }
 
 
+
 restart_app_prompt(){
-    prompt_app_selection "ALL"
-    app_name=$(echo "${apps[app_index-1]}" | awk -F ',' '{print $1}')
-    
-    clear -x
-    title
+    while true; do
+        prompt_app_selection "ALL"
+        app_name=$(echo "${apps[app_index-1]}" | awk -F ',' '{print $1}')
+        
+        clear -x
+        title
 
-    echo -e "Restarting ${blue}$app_name${reset}..."
+        echo -e "Restarting ${blue}$app_name${reset}..."
 
-    if ! restart_app; then
-        echo -e "${red}Failed to restart ${blue}$app_name${reset}"
-    else
-        echo -e "${green}Restarted ${blue}$app_name${reset}"
-    fi
+        if ! restart_app; then
+            echo -e "${red}Failed to restart ${blue}$app_name${reset}"
+        else
+            echo -e "${green}Restarted ${blue}$app_name${reset}"
+        fi
+
+        read -rp "Would you like to restart another application? (y/N): " choice
+        case "$(echo "$choice" | tr '[:upper:]' '[:lower:]')" in
+            "yes"|"y")
+                continue
+                ;;
+            "no"|"n")
+                break
+                ;;
+            *)
+                echo -e "${red}Invalid choice, please enter ${blue}'y'${red} or ${blue}'n'${reset}"
+        esac
+    done
 }
 
 
+
 delete_app_prompt(){
-    prompt_app_selection "ALL"
-    app_name=$(echo "${apps[app_index-1]}" | awk -F ',' '{print $1}')
-    
-    clear -x
-    title
-
-    echo -e "Stopping ${blue}$app_name${reset}..."
-
-    echo -e "${bold}Chosen Application: ${blue}$app_name${reset}"
-    echo -e "${yellow}WARNING: This will delete the application and all associated data, including snapshots${reset}"
-    echo
     while true; do
-        read -rp "Continue with deletion?(y/N): " confirmation
-        case "$confirmation" in
-            y|Y)
-                if cli -c "app chart_release delete release_name=\"$app_name\""; then
-                    echo -e "${green}App $app_name deleted${reset}"
-                    exit
-                else
-                    echo -e "${red}Failed to delete app $app_name${reset}"
-                fi
-                exit
+        prompt_app_selection "ALL"
+        app_name=$(echo "${apps[app_index-1]}" | awk -F ',' '{print $1}')
+        
+        clear -x
+        title
+
+        echo -e "Stopping ${blue}$app_name${reset}..."
+
+        echo -e "${bold}Chosen Application: ${blue}$app_name${reset}"
+        echo -e "${yellow}WARNING: This will delete the application and all associated data, including snapshots${reset}"
+        echo
+        while true; do
+            read -rp "Continue with deletion?(y/n): " confirmation
+            case "$(echo "$confirmation" | tr '[:upper:]' '[:lower:]')" in
+                "y")
+                    if cli -c "app chart_release delete release_name=\"$app_name\""; then
+                        echo -e "${green}App $app_name deleted${reset}"
+                    else
+                        echo -e "${red}Failed to delete app $app_name${reset}"
+                    fi
+                    break
+                    ;;
+                "n")
+                    echo -e "Exiting.."
+                    break
+                    ;;
+                *)
+                    echo -e "${red}Invalid option. Please enter 'y' or 'n'.${reset}"
+                    sleep 3
+                    ;;
+            esac
+        done
+        read -rp "Would you like to delete another application? (y/n): " choice
+        case "$(echo "$choice" | tr '[:upper:]' '[:lower:]')" in
+            "y")
+                continue
                 ;;
-            n|N)
-                echo -e "Exiting.."
-                exit
+            "n")
+                break
                 ;;
             *)
-                echo -e "${red}Invalid option. Please enter 'y' or 'n'.${reset}"
-                sleep 3
-                ;;
+                echo "Invalid choice, please enter 'y' or 'n'"
         esac
     done
 }
 
 
 stop_app_prompt(){
-    prompt_app_selection "ACTIVE"
-    app_name=$(echo "${apps[app_index-1]}" | awk -F ',' '{print $1}')
-    
-    clear -x
-    title
-    
-    echo -e "Stopping ${blue}$app_name${reset}..."
+    while true; do
+        prompt_app_selection "ACTIVE"
+        app_name=$(echo "${apps[app_index-1]}" | awk -F ',' '{print $1}')
+        
+        clear -x
+        title
+        
+        echo -e "Stopping ${blue}$app_name${reset}..."
 
-    if ! cli -c 'app chart_release scale release_name='\""$app_name"\"\ 'scale_options={"replica_count": 0}' &> /dev/null; then
-        echo -e "${red}Failed to stop ${blue}$app_name${reset}"
-        exit 1
-    else
-        echo -e "${blue}$app_name ${green}Stopped${reset}"
-    fi
+        if ! cli -c 'app chart_release scale release_name='\""$app_name"\"\ 'scale_options={"replica_count": 0}' &> /dev/null; then
+            echo -e "${red}Failed to stop ${blue}$app_name${reset}"
+        else
+            echo -e "${blue}$app_name ${green}Stopped${reset}"
+        fi
+
+        read -rp "Would you like to stop another application? (y/n): " choice
+        case "$(echo "$choice" | tr '[:upper:]' '[:lower:]')" in
+            "y")
+                continue
+                ;;
+            "n")
+                break
+                ;;
+            *)
+                echo "Invalid choice, please enter 'y' or 'n'"
+        esac
+    done
 }
+
 
 
 start_app_prompt(){
-    prompt_app_selection "STOPPED"
-    app_name=$(echo "${apps[app_index-1]}" | awk -F ',' '{print $1}')
-    
-    clear -x
-    title
+    while true; do
+        prompt_app_selection "STOPPED"
+        app_name=$(echo "${apps[app_index-1]}" | awk -F ',' '{print $1}')
+        
+        clear -x
+        title
 
-    # Pull chart info
-    initial_call=$(midclt call chart.release.get_instance "$app_name")
+        # Pull chart info
+        initial_call=$(midclt call chart.release.get_instance "$app_name")
 
-    # query chart name
-    query_name=$(echo "$initial_call" | jq .chart_metadata.name | 
-                sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+        # query chart name
+        query_name=$(echo "$initial_call" | jq .chart_metadata.name | 
+                    sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
 
-    # check if chart is an external service
-    if [[ $query_name == "external-service" ]]; then
-        echo -e "${blue}$app_name${red} is an external service.${reset}"
-        echo -e "${red}These application types cannot be started.${reset}"
-        exit 1
-    # check if chart is an ix-chart
-    elif [[ $query_name == "ix-chart" ]]; then
-        echo -e "${blue}$app_name${red} is an ix-chart.${reset}"
-        echo -e "${red}These application types do not have a replica assigned to them.${reset}"
-        echo -e "${red}As of now these appliaction types are unsupported.${reset}"
-        exit 1
-    fi
+        # check if chart is an external service
+        if [[ $query_name == "external-service" ]]; then
+            echo -e "${blue}$app_name${red} is an external service.${reset}"
+            echo -e "${red}These application types cannot be started.${reset}"
+            break
+        # check if chart is an ix-chart
+        elif [[ $query_name == "ix-chart" ]]; then
+            echo -e "${blue}$app_name${red} is an ix-chart.${reset}"
+            echo -e "${red}These application types do not have a replica assigned to them.${reset}"
+            echo -e "${red}As of now these application types are unsupported.${reset}"
+            break
+        fi
 
-    # query chosen replica count for application
-    replica_count=$(echo "$initial_call" | jq .config.controller.replicas | 
-                sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
+        # query chosen replica count for application
+        replica_count=$(echo "$initial_call" | jq .config.controller.replicas | 
+                    sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')
 
-    echo -e "Starting ${blue}$app_name${reset}..."
+        echo -e "Starting ${blue}$app_name${reset}..."
 
-    # Start application with chosen replica count
-    if cli -c 'app chart_release scale release_name='\""$app_name"\"\ 'scale_options={"replica_count": '"$replica_count}" &> /dev/null; then
-        echo -e "${blue}$app_name ${green}Started${reset}"
-        echo -e "${green}Replica count set to ${blue}$replica_count${reset}"
-        return 0
-    else
-        echo -e "${red}Failed to start ${blue}$app_name${reset}"
-        return 1
-    fi
+        # Start application with chosen replica count
+        if cli -c 'app chart_release scale release_name='\""$app_name"\"\ 'scale_options={"replica_count": '"$replica_count}" &> /dev/null; then
+            echo -e "${blue}$app_name ${green}Started${reset}"
+            echo -e "${green}Replica count set to ${blue}$replica_count${reset}"
+        else
+            echo -e "${red}Failed to start ${blue}$app_name${reset}"
+        fi
 
+        read -rp "Would you like to start another application? (y/n): " choice
+        case "$(echo "$choice" | tr '[:upper:]' '[:lower:]')" in
+            "y")
+                continue
+                ;;
+            "n")
+                break
+                ;;
+            *)
+                echo "Invalid choice, please enter 'y' or 'n'"
+        esac
+    done
 }
+

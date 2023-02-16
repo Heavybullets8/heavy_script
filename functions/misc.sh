649ea9b7ad6c072c6f0f0b00e96e48c0008abe2b
heavybullets8 <heavybullets8@gmail.com>
2023-02-16 01:27:32 -0700

    change prune for v23 of truenas

--- functions/misc.sh
+++ functions/misc.sh
@@ -22,7 +22,13 @@ prune(){
                awk -F '-' '{print $3}' | 
                awk -F '.' '{print $1 $2}' |  
                tr -d " \t\r\.")"
-    if (( "$version" >= 2212 )); then
+    if (( "$version" >= 2310 )); then
+        if ! cli -c 'app container config prune prune_options={"remove_unused_images": true}' &>/dev/null ; then
+            echo -e "Failed to Prune Docker Images"
+        else
+            echo -e "Pruned Docker Images"
+        fi
+    elif (( "$version" >= 2212 )); then
         if ! cli -c 'app container config prune prune_options={"remove_unused_images": true, "remove_stopped_containers": true}' | head -n -4; then
             echo -e "Failed to Prune Docker Images"
         fi

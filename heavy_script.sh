c06e7551ea86fbebd4d36f4dacabe8f7769617a7
heavybullets8 <heavybullets8@gmail.com>
2023-03-26 17:53:09 -0600

    remove shift

--- heavy_script.sh
+++ heavy_script.sh
@@ -74,37 +74,30 @@ self_update_handler "${args[@]}"
 while [[ "${#args[@]}" -gt 0 ]]; do
   case $1 in
     app)
-      shift # Remove 'app' from the arguments
       app_handler "${args[@]:1}" # Pass remaining arguments to app_handler
       exit
       ;;
     backup)
-      shift # Remove 'backup' from the arguments
       backup_handler "${args[@]:1}" # Pass remaining arguments to backup_handler
       exit
       ;;
     dns)
-      shift # Remove 'dns' from the arguments
       dns_handler "${args[@]:1}" # Pass remaining arguments to dns_handler
       exit
       ;;
     git)
-      shift # Remove 'git' from the arguments
       git_handler "${args[@]:1}" # Pass remaining arguments to git_handler
       exit
       ;;
     pod)
-      shift # Remove 'pod' from the arguments
       pod_handler "${args[@]:1}" # Pass remaining arguments to pod_handler
       exit
       ;;
     pvc)
-      shift # Remove 'mount' from the arguments
       mount_handler "${args[@]:1}" # Pass remaining arguments to mount_handler
       exit
       ;;
     update)
-      shift # Remove 'update' from the arguments
       update_handler "${args[@]:1}" # Pass remaining arguments to update_handler
       exit
       ;;

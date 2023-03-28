3abb7ffc61e30e9a92afb1067ede282c6d5ed545
heavybullets8 <heavybullets8@gmail.com>
2023-03-27 22:25:10 -0600

    only open help if no self_update

--- functions/self_update/handler.sh
+++ functions/self_update/handler.sh
@@ -2,31 +2,33 @@
 
 
 self_update_handler() {
-  local args=("$@")
-  local self_update=false
-  local no_self_update=false
-  local include_major=false
+    local args=("$@")
+    local self_update=false
+    local no_self_update=false
+    local include_major=false
 
- # Check if second element is a --help/-h argument
-    if [[ "${args[1]}" == "--help" || "${args[1]}" == "-h" ]]; then
+    for arg in "${args[@]}"; do
+        if [[ "$arg" =~ ^(--)?self-update$ || "$arg" == "-U" ]]; then
+            self_update=true
+        elif [[ "$arg" == "--no-self-update" ]]; then
+            no_self_update=true
+            break
+        elif [[ "$arg" == "--major" ]]; then
+            include_major=true
+        fi
+    done
+
+
+    # Check if second element is a --help/-h argument and self_update is not equal to true
+    if [[ "${args[1]}" =~ ^(--help|-h)$ ]] && [[ "${self_update}" == "false" ]]; then
         self_update_help
         exit
     fi
 
-  for arg in "${args[@]}"; do
-      if [[ "$arg" =~ ^(--)?self-update$ || "$arg" == "-U" ]]; then
-          self_update=true
-      elif [[ "$arg" == "--no-self-update" ]]; then
-          no_self_update=true
-          break
-      elif [[ "$arg" == "--major" ]]; then
-          include_major=true
-      fi
-  done
 
-  if $self_update && ! $no_self_update; then
-      self_update 
-  fi
+    if $self_update && ! $no_self_update; then
+        self_update 
+    fi
 
 
     # Clean args

55784065fa22a30c342958985586b317615b6969
Heavybullets8 <heavybullets8@gmail.com>
2023-04-15 20:59:11 -0600

    update help to show new options

--- functions/app/help.sh
+++ functions/app/help.sh
@@ -4,7 +4,7 @@
 app_help() {
     echo -e "${bold}App Handler${reset}"
     echo -e "${bold}-----------${reset}"
-    echo -e "${blue}heavyscript app [Option]${reset}"
+    echo -e "${blue}heavyscript app [Option] [app_name]${reset}"
     echo
     echo -e "${bold}Description${reset}"
     echo -e "${bold}-----------${reset}"
@@ -12,16 +12,17 @@ app_help() {
     echo
     echo -e "${bold}Options${reset}"
     echo -e "${bold}-------${reset}"
-    echo -e "${blue}-s${reset}, ${blue}--start${reset}"
-    echo -e "    Start an application."
-    echo -e "${blue}-x${reset}, ${blue}--stop${reset}"
-    echo -e "    Stop an application."
-    echo -e "${blue}-r${reset}, ${blue}--restart${reset}"
-    echo -e "    Restart an application."
-    echo -e "${blue}-d${reset}, ${blue}--delete${reset}"
-    echo -e "    Delete an application."
+    echo -e "${blue}-s${reset}, ${blue}--start${reset} [app_name]"
+    echo -e "    Start an application. If no app_name is provided, the user will be prompted to select one."
+    echo -e "${blue}-x${reset}, ${blue}--stop${reset} [app_name]"
+    echo -e "    Stop an application. If no app_name is provided, the user will be prompted to select one."
+    echo -e "${blue}-r${reset}, ${blue}--restart${reset} [app_name]"
+    echo -e "    Restart an application. If no app_name is provided, the user will be prompted to select one."
+    echo -e "${blue}-d${reset}, ${blue}--delete${reset} [app_name]"
+    echo -e "    Delete an application. If no app_name is provided, the user will be prompted to select one."
     echo -e "${blue}-h${reset}, ${blue}--help${reset}"
     echo -e "    Display this help message."
     echo
 }
+
 

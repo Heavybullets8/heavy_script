52da9c6a56a3108f5c17729c6eb5b10893d7c5d0
heavybullets8 <heavybullets8@gmail.com>
2023-04-18 02:48:05 -0600

    test

--- utils/unset_from_array.sh
+++ utils/unset_from_array.sh
@@ -3,7 +3,7 @@
 remove_self_update_args() {
     local input_args=("$@")
     local output_args=()
-    export self_update=false
+    export self_update
 
     for arg in "${input_args[@]}"; do
         if [[ "$arg" =~ ^(--)?self-update$ || "$arg" == "-U" ]]; then
@@ -20,7 +20,7 @@ remove_self_update_args() {
 remove_no_self_update_args() {
     local input_args=("$@")
     local output_args=()
-    export no_self_update=false
+    export no_self_update
 
     for arg in "${input_args[@]}"; do
         if [[ "$arg" == "--no-self-update" ]]; then
@@ -37,7 +37,7 @@ remove_no_self_update_args() {
 remove_force_update_args() {
     local input_args=("$@")
     local output_args=()
-    export major_self_update=false
+    export major_self_update
 
     for arg in "${input_args[@]}"; do
         if [[ "$arg" == "--major" ]]; then
@@ -54,7 +54,7 @@ remove_force_update_args() {
 remove_no_config_args() {
     local input_args=("$@")
     local output_args=()
-    export no_config=false
+    export no_config
 
     for arg in "${input_args[@]}"; do
         if [[ "$arg" == "--no-config" ]]; then

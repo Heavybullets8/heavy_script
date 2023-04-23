a2de29cb3294f9d3d6243e456e33d34cb706d05d
heavybullets8 <heavybullets8@gmail.com>
2023-04-23 11:57:30 -0600

    process skips fix

--- functions/update/commander.sh
+++ functions/update/commander.sh
@@ -40,47 +40,55 @@ display_update_status() {
     fi
 }
 
+# Skip if the app is in the ignore list
 skip_app_on_ignore_list() {
     if printf '%s\0' "${ignore[@]}" | grep -iFxqz "${app_name}"; then
         echo -e "\n$app_name\nIgnored, skipping"
-        unset "array[$index]"
+        return 0
     fi
+    return 1
 }
 
+# Skip if there's a major release
 skip_major_release() {
     if [[ "$old_app_ver" != "$new_app_ver" ]] && [[ "$old_chart_ver" != "$new_chart_ver" ]]; then
         echo -e "\n$app_name\nSkipping major app and chart release\n$old_full_ver\n$new_full_ver"
-        unset "array[$index]"
+        return 0
     elif [[ "$old_app_ver" != "$new_app_ver" ]]; then
         echo -e "\n$app_name\nSkipping major app release\n$old_full_ver\n$new_full_ver"
-        unset "array[$index]"
+        return 0
     elif [[ "$old_chart_ver" != "$new_chart_ver" ]]; then
         echo -e "\n$app_name\nSkipping major chart release\n$old_full_ver\n$new_full_ver"
-        unset "array[$index]"
+        return 0
     fi
+    return 1
 }
 
+# Skip if the app version has previously failed
 skip_previously_failed_version() {
     if grep -qs "^$app_name," failed 2>/dev/null; then
         failed_ver=$(grep "^$app_name," failed | awk -F ',' '{print $2}')
         if [[ "$failed_ver" == "$new_full_ver" ]]; then
             echo -e "\n$app_name\nSkipping previously failed version\n$new_full_ver"
-            unset "array[$index]"
+            return 0
         else
             sed -i /"$app_name",/d failed
         fi
     fi
+    return 1
 }
 
+# Skip if the image update should be ignored
 skip_image_update() {
     if [[ $old_full_ver == "$new_full_ver" && $ignore_image_update == true ]]; then
         echo -e "\n$app_name\nImage update, skipping.."
-        unset "array[$index]"
+        return 0
     fi
+    return 1
 }
 
 process_apps() {
-    index=0
+    local filtered_apps=()
 
     for app in "${array[@]}"; do
         # process each app and potentially remove it from the array
@@ -92,16 +100,28 @@ process_apps() {
         old_chart_ver=$(echo "$old_full_ver" | awk -F '_' '{print $2}' | awk -F '.' '{print $1}')
         new_chart_ver=$(echo "$new_full_ver" | awk -F '_' '{print $2}' | awk -F '.' '{print $1}')
 
-        skip_app_on_ignore_list
-        if [[ $update_all_apps != true ]]; then
-            skip_major_release
+        if skip_app_on_ignore_list; then
+            continue
         fi
-        skip_previously_failed_version
-        skip_image_update
 
-        ((index++))
+        if [[ $update_all_apps != true ]]; then
+            if skip_major_release; then
+                continue
+            fi
+        fi
+
+        if skip_previously_failed_version; then
+            continue
+        fi
+
+        if skip_image_update; then
+            continue
+        fi
+
+        # Add the app to the filtered_apps array if it passes all the conditions
+        filtered_apps+=("$app")
     done
-    array=("${array[@]}")
+    array=("${filtered_apps[@]}")
 }
 
 

66d7485d95a12a9d93e7ad09bff26277aa6988f5
Heavybullets8 <heavybullets8@gmail.com>
2023-05-01 02:43:42 -0600

    only scale deployments

--- utils/resources.sh
+++ utils/resources.sh
@@ -13,22 +13,10 @@ scale_resources() {
     timeout="$2"
     replicas="${3:-$(pull_replicas "$app_name")}"
 
-    apply_scaling() {
-        local resource_type
-        resource_type="$1"
-        k3s kubectl get "$resource_type" -n ix-"$app_name" -o json | jq -r '.items[] | select(.metadata.name | contains("-cnpg-main-") | not) | .metadata.name' | xargs -r -I{} k3s kubectl scale "$resource_type"/{} -n ix-"$app_name" --replicas="$replicas"
-    }
+    k3s kubectl get deployments -n ix-"$app_name" -o json | jq -r '.items[] | select(.metadata.name | contains("-cnpg-main-") | not) | .metadata.name' | xargs -r -I{} k3s kubectl scale deployments/{} -n ix-"$app_name" --replicas="$replicas"
 
     if [[ $replicas -eq 0 ]]; then
-        # Scale down all Deployments and StatefulSets in the namespace
-        apply_scaling "deployments"
-        apply_scaling "statefulsets"
-
         wait_for_pods_to_stop "$app_name" "$timeout" && return 0 || return 1
-    else
-        # Scale up all Deployments and StatefulSets in the namespace
-        apply_scaling "deployments"
-        apply_scaling "statefulsets"
     fi
 }
 

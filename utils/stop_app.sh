147d50a071a24e9698896500a3a845adcf736427
Heavybullets8 <heavybullets8@gmail.com>
2023-05-01 02:56:01 -0600

    fix

--- utils/stop_app.sh
+++ utils/stop_app.sh
@@ -7,7 +7,7 @@ wait_for_pods_to_stop() {
     timeout="$2"
 
     SECONDS=0
-    while k3s kubectl get pods -n ix-"$app_name" --field-selector=status.phase!=Succeeded,status.phase!=Failed -o=name | grep -v -- '-cnpg-' | grep -vE -- '-\d$'; do
+    while k3s kubectl get pods -n ix-"$app_name" --field-selector=status.phase!=Succeeded,status.phase!=Failed -o=name | grep -v -- '-cnpg-' | grep -vqE -- '-[[:digit:]]$'; do
         if [[ "$SECONDS" -gt $timeout ]]; then
             return 1
         fi

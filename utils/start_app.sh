895d75fda8350b2f420fb72d7426ae8b47930de8
Heavybullets8 <heavybullets8@gmail.com>
2023-05-31 20:49:25 -0600

    add grep check to start function

--- utils/start_app.sh
+++ utils/start_app.sh
@@ -3,11 +3,10 @@
 start_app(){
     local app_name=$1
     local replica_count=$2
-    
+
     # Check if app is a cnpg instance, or an operator instance
     output=$(check_filtered_apps "$app_name")
-
-    if [[ $output == "${app_name},stopAll-on" ]]; then
+    if echo "$output" | grep -q "${app_name},stopAll-on"; then
         if ! cli -c 'app chart_release scale release_name='\""$app_name"\"\ 'scale_options={"replica_count": '"$replica_count}" > /dev/null; then
             return 1
         fi

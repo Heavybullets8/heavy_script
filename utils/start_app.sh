9a96cc2d084d1e73d4f78a47f906cdfdafe7c3a7
Heavybullets8 <heavybullets8@gmail.com>
2023-06-11 13:49:42 -0600

    test slightly different start method

--- utils/start_app.sh
+++ utils/start_app.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 abort_job(){
-    # Get the job ID
-    job_id=$(get_running_job_id "$app_name")
+    local app_name=$1
+    job_id=""
 
-    # Abort the job if found
-    midclt call core.job_abort "$job_id" > /dev/null 2>&1
+    # shellcheck disable=SC2034
+    for i in {1..60}; do
+        job_id=$(get_running_job_id "$app_name")
 
+        if [[ -n "$job_id" ]]; then
+            midclt call core.job_abort "$job_id" > /dev/null 2>&1
+            return 0
+        fi
+
+        sleep 1
+    done
+    return 1
 }
 
 get_running_job_id(){
@@ -23,13 +32,14 @@ start_app(){
     # Check if app is a cnpg instance, or an operator instance
     output=$(check_filtered_apps "$app_name")
     if [[ $output == *"${app_name},stopAll-on"* ]]; then
-        if ! cli -c "app chart_release update chart_release=\"$app_name\" values={\"global\": {\"stopAll\": false}}" > /dev/null 2>&1; then
-            return 1
-        fi
+        midclt call chart.release.update "$app_name" '{"values": {"global": {"stopAll": false}}}'
+        # if ! cli -c "app chart_release update chart_release=\"$app_name\" values={\"global\": {\"stopAll\": false}}" > /dev/null 2>&1; then
+        #     return 1
+        # fi
         abort_job "$app_name"
-        job_id=$(midclt call chart.release.scale "$app_name" '{"replica_count": '"$replica_count"'}') || return 1
-        sleep 5
-        midclt call core.job_abort "$job_id" > /dev/null 2>&1
+        # job_id=$(midclt call chart.release.scale "$app_name" '{"replica_count": '"$replica_count"'}') || return 1
+        # sleep 5
+        # midclt call core.job_abort "$job_id" > /dev/null 2>&1
     elif [[ $output == *"${app_name},stopAll-off"* ]]; then
         job_id=$(midclt call chart.release.scale "$app_name" '{"replica_count": '"$replica_count"'}') || return 1
         sleep 5

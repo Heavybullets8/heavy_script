1c3b66c6b9df3deb7d71fdb1640060cd625773a7
heavybullets8 <heavybullets8@gmail.com>
2023-08-16 18:01:22 -0600

    init

--- functions/enable/help.sh
+++ functions/enable/help.sh
@@ -2,20 +2,23 @@
 
 
 enable_help() {
-    echo -e "${bold}Enable Handler${reset}"
-    echo -e "${bold}-------------${reset}"
+    echo -e "${bold}Enable/Disable Handler${reset}"
+    echo -e "${bold}---------------------${reset}"
     echo -e "${blue}heavyscript enable | ${blue}[FEATURE]${reset}"
+    echo -e "${blue}heavyscript enable | ${blue}--disable-[FEATURE]${reset}"
     echo
     echo -e "${bold}Description${reset}"
     echo -e "${bold}-----------${reset}"
-    echo -e "    Enable specific locked down functions in Truenas SCALE."
+    echo -e "    Enable or disable specific locked-down functions in TrueNAS SCALE."
     echo
     echo -e "${bold}FEATURES${reset}"
     echo -e "${bold}-------${reset}"
     echo -e "${blue}--api${reset}"
     echo -e "    Enables external access to the Kubernetes API server"
     echo -e "${blue}--apt${reset}"
-    echo -e "    Enable apt, apt-get, and apt-key."
+    echo -e "    Enable apt, apt-get, apt-key and dpkg."
+    echo -e "${blue}--disable-apt${reset}"  
+    echo -e "    Disable apt, apt-get, apt-key, and dpkg."  
     echo -e "${blue}--helm${reset}"
     echo -e "    Enable helm commands."
     echo -e "${blue}-h, --help${reset}"
@@ -25,5 +28,6 @@ enable_help() {
     echo -e "${bold}-------${reset}"
     echo -e "    ${blue}heavyscript enable --api${reset}"
     echo -e "    ${blue}heavyscript enable --apt${reset}"
+    echo -e "    ${blue}heavyscript enable --disable-apt${reset}"
     echo
 }

bc70df7bba02f25ac2286388543d6509b580e273
heavybullets8 <heavybullets8@gmail.com>
2023-08-26 17:30:12 -0600

    remove desired replicas

--- utils/resources.sh
+++ utils/resources.sh
@@ -4,7 +4,7 @@ pull_replicas() {
     local app_name
     app_name="$1"
 
-    midclt call chart.release.get_instance "$app_name" | jq '.config.controller.replicas // .config.workload.main.replicas // .pod_status.desired'
+    midclt call chart.release.get_instance "$app_name" | jq '.config.controller.replicas // .config.workload.main.replicas'
 }
 
 restart_app(){

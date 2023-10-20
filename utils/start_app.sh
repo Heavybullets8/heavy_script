41e6599a56dc5740b25299283934dc2020fc2710
Heavy <heavybullets8@gmail.com>
2023-10-19 22:36:32 -0600

    use kube file flag instead of var

--- utils/start_app.sh
+++ utils/start_app.sh
@@ -26,29 +26,21 @@ start_app(){
             return 1
         fi
     elif [[ $output == *"${app_name},stopAll-"* ]]; then
-        # Get app pool path
         ix_apps_pool=$(get_apps_pool)
 
-        # Get latest version
         latest_version=$(find "/mnt/$ix_apps_pool/ix-applications/releases/$app_name/charts" -mindepth 1 -maxdepth 1 -type d | 
                         awk -F'/' '{print $NF}' | 
                         sort -V | 
                         tail -n 1)
 
-
-        # Helm upgrade command
-        if ! KUBECONFIG="/etc/rancher/k3s/k3s.yaml" \
-            helm upgrade -n "ix-$app_name" "$app_name" \
+        if ! helm upgrade -n "ix-$app_name" "$app_name" \
             "/mnt/$ix_apps_pool/ix-applications/releases/$app_name/charts/$latest_version" \
+            --kubeconfig "/etc/rancher/k3s/k3s.yaml" \
             --reuse-values \
-            --set global.stopAll=false; then 
+            --set global.stopAll=false > /dev/null 2>&1; then 
             return 1
         fi
 
-    # elif [[ $output == *"${app_name},stopAll-off"* ]]; then
-    #     job_id=$(midclt call chart.release.redeploy_internal "$app_name") || return 1
-    #     wait_for_redeploy_methods "$app_name"
-    #     midclt call core.job_abort "$job_id" > /dev/null 2>&1
     else
         replicas=$(pull_replicas "$app_name")
         if ! cli -c 'app chart_release scale release_name='\""$app_name"\"\ 'scale_options={"replica_count": '"$replicas}" > /dev/null 2>&1; then

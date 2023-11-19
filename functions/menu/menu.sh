6aab02a2a605222889369c643046b05bf4a89413
Heavybullets8 <heavybullets8@gmail.com>
2023-11-19 12:13:04 -0700

    Refactor menu function to use a while loop

--- functions/menu/menu.sh
+++ functions/menu/menu.sh
@@ -9,46 +9,44 @@ menu_check_root() {
     fi
 }
 
-menu(){
-    clear -x
-    title
-    echo -e "${bold}Available Utilities${reset}"
-    echo -e "${bold}-------------------${reset}"
-    echo -e "1)  Help"
-    echo -e "2)  Application Options"
-    echo -e "3)  Backup Options"
-    echo -e "4)  HeavyScript Options"
-    echo
-    echo -e "0)  Exit"
-    read -rt 120 -p "Please select an option by number: " selection || { echo -e "${red}\nFailed to make a selection in time${reset}" ; exit; }
+menu() {
+    while true; do
+        clear -x
+        title
+        echo -e "${bold}Available Utilities${reset}"
+        echo -e "${bold}-------------------${reset}"
+        echo -e "1)  Help"
+        echo -e "2)  Application Options"
+        echo -e "3)  Backup Options"
+        echo -e "4)  HeavyScript Options"
+        echo
+        echo -e "0)  Exit"
+        read -rt 120 -p "Please select an option by number: " selection || { echo -e "${red}\nFailed to make a selection in time${reset}" ; exit; }
 
-
-    case $selection in
-        0)
-            echo -e "Exiting.."
-            exit
-            ;;
-        1)
-            main_help
-            exit
-            ;;
-        2) # Applicaiton Options
-            menu_check_root
-            application_menu
-            ;;
-        3) # Backup Options
-            menu_check_root
-            backup_selection
-            ;;
-        4) # HeavyScript Options
-            heavyscript_menu
-            ;;
-        *)
-            echo -e "${blue}\"$selection\"${red} was not an option, please try again${reset}"
-            sleep 3
-            menu
-            ;;
-    esac
-    echo
+        case $selection in
+            0)
+                echo -e "Exiting.."
+                exit
+                ;;
+            1)
+                main_help
+                ;;
+            2) # Application Options
+                menu_check_root
+                application_menu
+                ;;
+            3) # Backup Options
+                menu_check_root
+                backup_selection
+                ;;
+            4) # HeavyScript Options
+                heavyscript_menu
+                ;;
+            *)
+                echo -e "${blue}\"$selection\"${red} was not an option, please try again${reset}"
+                sleep 3
+                ;;
+        esac
+    done
 }
 export -f menu

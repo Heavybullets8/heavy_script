1baf02c1775e049ef27d4433c437ce40184815ee
Heavy <heavybullets8@gmail.com>
2024-01-10 01:51:10 -0700

    Refactor generate_config.sh: Remove add_database_options function

--- utils/generate_config.sh
+++ utils/generate_config.sh
@@ -10,16 +10,6 @@ generate_config_ini() {
     fi
 }
 
-add_database_options() {
-    local config_file="config.ini"
-
-    # Check if the stop_before_dump option exists
-    if ! grep -q "^stop_before_dump=" "$config_file"; then
-        # Add the stop_before_dump option with a default value and description
-        awk -i inplace -v stop_before_dump_option="\n# Apps listed here will have their deployments shut down prior to their CNPG Database dump\n# This is usually unnecessary, and unless otherwise recommended, leave blank\n# Example: stop_before_dump=nextcloud,appname,appname\nstop_before_dump=\"\"\n" '/^dump_folder=.*/ { print; print stop_before_dump_option; next }1' "$config_file"
-    fi
-}
-
 remove_dns_section() {
     local config_file="config.ini"
 

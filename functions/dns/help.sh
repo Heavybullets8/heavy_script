26f1fab788af1c05eb46db997c023c180dcc4019
Heavy <heavybullets8@gmail.com>
2024-01-10 02:08:58 -0700

    Update DNS help message to include app name filtering

--- functions/dns/help.sh
+++ functions/dns/help.sh
@@ -4,20 +4,28 @@
 dns_help() {
     echo -e "${bold}DNS Handler${reset}"
     echo -e "${bold}-----------${reset}"
-    echo -e "${blue}heavyscript dns | ${blue}[AppName]${reset}"
+    echo -e "${blue}heavyscript dns | ${blue}[AppNames]${reset}"
     echo
     echo -e "${bold}Description${reset}"
     echo -e "${bold}-----------${reset}"
-    echo -e "List all ix DNS records"
+    echo -e "List all ix DNS records. Optionally, specify one or more app names to list DNS records for those specific apps."
     echo
     echo -e "${bold}Options${reset}"
     echo -e "${bold}-------${reset}"
     echo -e "${blue}AppName${reset}"
+    echo -e "    Pass one or more app names (separated by spaces) to filter DNS records for those apps."
     echo -e "${blue}--no-config${reset}"
     echo -e "    Ignore the settings in your config.ini file."
     echo -e "${blue}-h${reset}, ${blue}--help${reset}"
     echo -e "    Display this help message."
     echo
+    echo -e "${bold}Example${reset}"
+    echo -e "${bold}-------${reset}"
+    echo -e "    ${blue}heavyscript dns sonarr${reset}"
+    echo -e "    ${blue}heavyscript dns radarr nextcloud${reset}"
+    echo -e "    ${blue}heavyscript dns${reset} - Displays DNS records for all apps"
+    echo
 }
+
 
 

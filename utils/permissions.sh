a597c972e960a4f5bea8bee1c7b8eae69d5e4593
Heavy <heavybullets8@gmail.com>
2024-04-29 00:53:26 -0600

    Update sudoers file to ensure necessary configuration for script execution

--- utils/permissions.sh
+++ utils/permissions.sh
@@ -17,13 +17,63 @@ check_root() {
     fi
 }
 
-ensure_symlink() {
-    local script_location="$script_path/bin/heavyscript"
+ensure_sudoers() {
+    get_invoking_user() {
+        if [[ $EUID -eq 0 ]]; then
+            echo "${SUDO_USER:-root}"
+        else
+            whoami
+        fi
+    }
 
-    if ! grep -q "$script_location" "/root/$rc_file"; then
-        echo -e "${blue}Adding $script_location to /root/$rc_file...${reset}"
-        echo "export PATH=$script_location:\$PATH" >> "/root/$rc_file"
+    local user env_keep_exists secure_path_exists TMP_FILE
+
+    local script_location="$script_path/bin/heavyscript"
+    user=$(get_invoking_user)
+
+    # No need to modify sudoers if the script is run as root
+    if [[ $user == "root" ]]; then
+        return 0
+    fi
+
+    # Check if specific entries already exist in the sudoers file
+    env_keep_exists=$(grep -c "Defaults:$user env_keep+=\"PATH\"" /etc/sudoers)
+    secure_path_exists=$(grep -c "Defaults:$user secure_path=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$script_location\"" /etc/sudoers)
+
+    # Exit the function if no changes are needed
+    if [[ $env_keep_exists -gt 0 && $secure_path_exists -gt 0 ]]; then
+        echo "No updates needed for sudoers file."
+        return 0
+    fi
+
+    # Create a temporary file only if changes are needed
+    TMP_FILE=$(mktemp)
+    
+    # Ensure cleanup on exit
+    trap 'rm -f "$TMP_FILE"' EXIT
+
+    # Copy current sudoers to temp
+    cat /etc/sudoers > "$TMP_FILE"
+
+    # Append new lines only if they don't already exist
+    if [[ $env_keep_exists -eq 0 ]]; then
+        echo "Defaults:$user env_keep+=\"PATH\"" >> "$TMP_FILE"
+    fi
+
+    if [[ $secure_path_exists -eq 0 ]]; then
+        echo "Defaults:$user secure_path=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$script_location\"" >> "$TMP_FILE"
+    fi
+
+    # Check for syntax errors with visudo
+    if visudo -c -f "$TMP_FILE"; then
+        # If the temp file is okay, safely copy it to the actual sudoers file
+        sudo cp "$TMP_FILE" /etc/sudoers
+        echo "Sudoers file has been updated successfully."
+    else
+        echo "Error found in sudoers temp file. No changes were made."
     fi
 }
+
+
 
 
